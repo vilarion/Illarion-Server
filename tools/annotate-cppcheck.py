e4b9062df3d65c05bd5f8df122b4c5d4397eae32
vilarion <vilarion@illarion.org>
2020-11-18 18:33:27 +0100

    Annotate PR with clang-tidy

--- tools/annotate-cppcheck.py
+++ tools/annotate-cppcheck.py
@@ -1,6 +1,5 @@
 import xml.etree.ElementTree as ET
-import requests
-import os
+import github
 
 annotations = []
 errors = 0
@@ -18,24 +17,5 @@ for error_tag in root.findall('errors/error'):
                             'message': error_tag.get('verbose'),
                             'title': error_tag.get('msg') + ' [' + error_tag.get('id') + ']'
                           })
-if errors > 0:
-    payload = {'output': {'title': 'Cppcheck Results', 'summary': str(errors) + ' errors detected.', 'annotations': annotations}}
 
-    repo = os.environ['GITHUB_REPOSITORY']
-    job_id = os.environ['JOB_ID']
-    run_id = os.environ['GITHUB_RUN_ID']
-    github_token = os.environ['GITHUB_TOKEN']
-
-    headers = {'Accept': 'application/vnd.github.v3+json',
-               'Authorization': 'token ' + github_token}
-    url = 'https://api.github.com/repos/' + repo + '/actions/runs/' + str(run_id)
-    r = requests.get(url, headers=headers)
-    check_suite_url = r.json()['check_suite_url']
-
-    url = check_suite_url + '/check-runs'
-    r = requests.get(url, headers=headers)
-    check_run = next((item for item in r.json()['check_runs'] if item['name'] == job_id), None)
-
-    url = 'https://api.github.com/repos/' + repo + '/check-runs/' + str(check_run['id'])
-    r = requests.patch(url, headers=headers, json=payload)
-    print(r.status_code)
+github.annotate('Cppcheck Results', errors, annotations)
